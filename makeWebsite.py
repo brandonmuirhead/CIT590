b7c0bae97c2d8e5ccebf7503939d493db3cccf8b
brandonmuirhead <bkm125@gmail.com>
2015-02-19 00:47:41 -0500

    Reading the text file complete; unit tests written

--- makeWebsite.py
+++ makeWebsite.py
@@ -14,13 +14,77 @@ def detectName(filename):
 		return 'The first line has to be the name with proper capitalization.'
 	else:
 		f.seek(0)
-		name=f.readline()
-		name=name.rstrip('\r\n')
+		name = f.readline()
+		name = name.rstrip('\r\n')
+		f.close()
 		return name
 	
+def detectEmail(filename):
+	'''Extracts email address from a text file. Only allows .com and .edu endings.'''
+	f = open(filename)
+	emailLine = ''
+	for line in f:
+		# check for '@' and '.edu' or '.com' in the line
+		if all(char in line for char in '@.edu') or all(char in line for char in '@.com'):
+			emailLine = line.rstrip('\r\n')
+	f.close()
+	if emailLine[:1] in string.ascii_lowercase:		#checks first character to make sure it is lowercase
+		return emailLine
 
+def detectCourse(filename):
+	'''Extracts course list from resume text file.'''
+	f = open(filename)
+	course = ''
+	for line in f:
+		if 'Course' in line:
+			course = line.rstrip('\r\n')
+	f.close()
+	courses = course.translate(None, '!?./;-:')	#removes any punctuation except commas
+	courses = " ".join(courses.split())	#gets rid of extra whitespace
+	return courses	#string
 
+def detectProject(filename):
+	'''Extracts projects from resume text file.'''
+	fileList = []
+	projList = []
+	f = open(filename, 'r')
+	for line in f:
+		fileList.append(line)	#convert file to list
+	f.close()
+	index = fileList.index('Projects\n')
+	while fileList[index+1][:10] != str("----------"):
+		projList.append(fileList[index+1])
+		index += 1
+	print projList
+	projList = remove_rn(projList)	#remove \r\n endings
+	projList = filter(None, projList)	#remove empty strings from list
+	return projList
 
+def detectEducation(filename):
+	'''Extracts lines from a file with information about schools and degrees earned.'''
+	ed1 = checkEd(filename, 'Bachelor')
+	ed2 = checkEd(filename, 'Master')
+	ed3 = checkEd(filename, 'Doctor')	
+	education = ed1 + ed2 + ed3
+	education = education.rstrip(' \r\n')
+	return education
 
-if __name__=='__main__':
-    print detectName('testresume.txt')
+def remove_rn(list):
+	'''Removes '\r\n' and similar endings from a list'''
+	clean = string.maketrans("", "")
+	list = [s.translate(clean, "\r\n") for s in list]
+	return list	
+
+def checkEd(file, degree):
+	'''Loops through file and checks for specific degree.'''
+	f = open(file)
+	education = ''
+	for line in f:
+		if 'University' in line and degree in line:
+			education += line
+	f.close()
+	return education
+
+		
+if __name__ == "__main__":
+	print detectEducation('testresume.txt')
--- makeWebsite_tests.py
+++ makeWebsite_tests.py
@@ -5,16 +5,43 @@ import unittest
 from makeWebsite import *
 
 class Test_makeWebsite(unittest.TestCase):
-    fileName = ''
+	fileName = ''
     
-    def setUp(self):
-        self.fileName = 'testresume.txt'
+	def setUp(self):
+		self.fileName = 'testresume.txt'
     
-    def test_detect_name(self):
-    	name = detectName(self.fileName)
-    	self.assertEqual("Brandon K. Muirhead",name)
-    	self.assertNotEqual('The first line has to be the name with proper capitalization.',name)
-        
-        
+	def test_detect_name(self):
+		name = detectName(self.fileName)
+		self.assertEqual("Brandon K. Muirhead",name)
+		self.assertNotEqual('The first line has to be the name with proper capitalization.',name)
+		#self.assertRaises(RuntimeError, detect_name("inasdf"))
+			
+	def test_detect_email(self):
+		email = detectEmail(self.fileName)
+		self.assertEqual('bmuir@wharton.upenn.edu', email)
+		self.assertNotEqual('Bmuir@wharton.upenn.edu', email)	#doesn't seem like the right test here. trying to test the first character not being capitalized
+		self.assertNotEqual('bmuir@wharton.upenn.EDU', email)
+    	
+	def test_detect_courses(self):
+		course = detectCourse(self.fileName)
+		self.assertEqual('Courses Programming Languages and Techniques, Statistics, Negotiations', course)
+		
+	def test_detect_projects(self):
+		projects = detectProject(self.fileName)
+		self.assertEqual(['Bill.com, Philadelphia, PA - Lead bill collector.', 'Army General - deployed platoon to Arizona where we saved the world.'], projects)
 
+	def test_detect_education(self):
+		education = detectEducation(self.fileName)
+		self.assertEqual('University of Penn, Town, PA, USA - Doctor of Coding', education)
+
+	def test_remove_rn(self):
+		remove = remove_rn(['foo\r', 'bar\r\n', 'foobar'])
+		self.assertEqual(['foo', 'bar', 'foobar'], remove)
+		self.assertEqual([], remove_rn([]))
+		
+	def test_check_ed(self):
+		edu = checkEd(self.fileName, 'Doctor')
+		self.assertEqual('University of Penn, Town, PA, USA - Doctor of Coding\r\n', edu)
+		
+		
 unittest.main()
