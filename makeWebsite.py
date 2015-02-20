1fbae29d18bfe153af04860171a555a8cd971c23
brandonmuirhead <bkm125@gmail.com>
2015-02-19 16:56:10 -0800

    Final program that takes a text file resume and converts it to html.

--- makeWebsite.py
+++ makeWebsite.py
@@ -1,7 +1,40 @@
 #makeWebsite.py
 #author Brandon Muirhead
 
+
+
 import string
+
+def main():
+	'''Function that creates an html file from a text resume.'''
+	f = open('resume.html', 'r+')
+	lines = f.readlines()
+	f.seek(0)
+	f.truncate()
+	del lines[-1]
+	del lines[-1]
+	f.writelines(lines)
+	f.write('<div id="page-wrap">')		#resume ready to insert text from .txt file
+
+	text = 'resume.txt'
+	intro = intro_section(text)
+	edu = edu_section(text)
+	proj = proj_section(text)
+	courses = course_section(text)
+	
+	f.write(intro)
+	f.write('\r\n')
+	f.write(edu)
+	f.write('\r\n')
+	f.write(proj)
+	f.write('\r\n')
+	f.write(courses)
+	f.write('\r\n')	
+	f.write('</div>\r\n</body>\r\n</html>')
+	
+	f.close()
+	
+	print 'Resume complete.'
 
 def detectName(filename):
 	'''Extracts first line from a resume.txt file, checks to ensure first character is A-Z'''
@@ -41,6 +74,7 @@ def detectCourse(filename):
 	f.close()
 	courses = course.translate(None, '!?./;-:')	#removes any punctuation except commas
 	courses = " ".join(courses.split())	#gets rid of extra whitespace
+	courses = courses.lstrip('Courses ')
 	return courses	#string
 
 def detectProject(filename):
@@ -55,26 +89,101 @@ def detectProject(filename):
 	while fileList[index+1][:10] != str("----------"):
 		projList.append(fileList[index+1])
 		index += 1
-	print projList
-	projList = remove_rn(projList)	#remove \r\n endings
-	projList = filter(None, projList)	#remove empty strings from list
+	projList = cleanList(projList)
 	return projList
 
 def detectEducation(filename):
 	'''Extracts lines from a file with information about schools and degrees earned.'''
-	ed1 = checkEd(filename, 'Bachelor')
+	ed1 = checkEd(filename, 'Doctor')
 	ed2 = checkEd(filename, 'Master')
-	ed3 = checkEd(filename, 'Doctor')	
-	education = ed1 + ed2 + ed3
-	education = education.rstrip(' \r\n')
-	return education
+	ed3 = checkEd(filename, 'Bachelor')	
+	education = [ed1, ed2, ed3]			#combines all degrees into a list
+	education = cleanList(education)	#removes any empty strings 
+	return education	#returns list
 
-def remove_rn(list):
-	'''Removes '\r\n' and similar endings from a list'''
+def intro_section(fileName):
+	'''Creates html code for intro section of resume.'''
+	nameText = detectName(fileName)
+	htmlName = surround_block('h1', nameText)	#name section of html
+	
+	emailText = detectEmail(fileName)
+	htmlEmail = surround_block('p', emailText)	#email section of html
+	
+	combine = combine_html(htmlName, htmlEmail)
+	
+	intro = surround_block('div', combine)
+	return intro
+
+def edu_section(fileName):
+	'''Creates html code for education section of resume.'''
+	header = surround_block('h2', 'Education')
+		
+	education = detectEducation(fileName)
+	edu1 = education[0]
+	edu2 = ''
+	edu3 = ''
+	if len(education) > 1:
+		edu2 = education[1]		#fills in edu2 if there are two degrees in file
+	if len(education) > 2:
+		edu3 = education[2]		#fills in edu3 if there are three degrees in file
+
+	edulist = [edu1, edu2, edu3]
+	edulist = cleanList(edulist)	
+	edulist = list_dict_html(edulist, 'li', '')
+
+	fullEdu = tie_body_header(header, edulist, 'ul')
+	return fullEdu		#html text
+
+def proj_section(fileName):
+	'''Creates html code for projects section of resume.'''
+	header = surround_block('h2', 'Projects')
+	projlist = detectProject(fileName)
+	projlist = list_dict_html(projlist, 'p', 'li')	
+	fullproj = tie_body_header(header, projlist, 'ul')
+	return fullproj		#html text
+
+def course_section(fileName):
+	'''Creates html code for course section of resume.'''
+	header = surround_block('h3', 'Courses')
+	courselist = detectCourse(fileName)
+	fullcourse = tie_body_header(header, courselist, 'span')
+	return fullcourse	#html text
+	
+def surround_block(tag, text):
+	'''surrounds some text in html block'''
+	html = '<' + tag + '>\r\n' + text + '\r\n</' + tag + '>'
+	return html	
+	
+def list_dict_html(list, innertag, outertag):
+	'''creates desired html with 1-2 tags from given list.'''
+	d = {}
+	clean = ''
+	count = 1
+	for x in list:
+		d[count] = surround_block(innertag, x)		#using dict lets me assign values to list in for loop
+		count += 1
+	
+	if len(d) == 1:
+		if outertag == '':							#just in case I want to put two tags before the text
+			clean = d[1]
+		else: 
+			clean = surround_block(outertag, d[1])
+	else: 
+		for i in range(1, len(d)+1):			#this is for a list with more than one item
+			if outertag != '':	
+				d[i] = surround_block(outertag, d[i])	#puts the second tag on both list items
+		for i in range(1, len(d)):	
+			clean = combine_html(d[i], d[i+1])	#combines everything into one html block
+			d[i+1] = clean
+	return clean
+
+def cleanList(list):
+	'''Removes '\r\n' and extra whitespace from a list.'''
 	clean = string.maketrans("", "")
-	list = [s.translate(clean, "\r\n") for s in list]
-	return list	
-
+	list = [s.translate(clean, "\r\n") for s in list]	#removes '\r\n' from list
+	list = filter(None, list)	#removes empty strings from list
+	return list
+	
 def checkEd(file, degree):
 	'''Loops through file and checks for specific degree.'''
 	f = open(file)
@@ -85,6 +194,17 @@ def checkEd(file, degree):
 	f.close()
 	return education
 
-		
+def combine_html(html1, html2):
+	'''Combines two strings	of html into one.'''
+	combine = html1 + '\r\n' + html2
+	return combine
+
+def tie_body_header(header, body, bodytag):
+	'''Ties together header and body for each section of resume.'''
+	section1 = surround_block(bodytag, body)
+	full = combine_html(header, section1)
+	fullhtml = surround_block('div', full)
+	return fullhtml
+
 if __name__ == "__main__":
-	print detectEducation('testresume.txt')
+	main()
--- makeWebsite_tests.py
+++ makeWebsite_tests.py
@@ -24,7 +24,7 @@ class Test_makeWebsite(unittest.TestCase):
     	
 	def test_detect_courses(self):
 		course = detectCourse(self.fileName)
-		self.assertEqual('Courses Programming Languages and Techniques, Statistics, Negotiations', course)
+		self.assertEqual('Programming Languages and Techniques, Statistics, Negotiations', course)
 		
 	def test_detect_projects(self):
 		projects = detectProject(self.fileName)
@@ -32,16 +32,58 @@ class Test_makeWebsite(unittest.TestCase):
 
 	def test_detect_education(self):
 		education = detectEducation(self.fileName)
-		self.assertEqual('University of Penn, Town, PA, USA - Doctor of Coding', education)
+		self.assertEqual(['University of Penn, Town, PA, USA - Doctor of Coding', 'Brigham Young University, Provo, UT, USA - Bachelor of Pinkberry'], education)
 
-	def test_remove_rn(self):
-		remove = remove_rn(['foo\r', 'bar\r\n', 'foobar'])
-		self.assertEqual(['foo', 'bar', 'foobar'], remove)
-		self.assertEqual([], remove_rn([]))
+	def test_intro_section(self):
+		intro = intro_section(self.fileName)
+		self.assertEqual('<div>\r\n<h1>\r\nBrandon K. Muirhead\r\n</h1>\r\n<p>\r\nbmuir@wharton.upenn.edu\r\n</p>\r\n</div>', intro)
+		
+	def test_edu_section(self):
+		edu = edu_section(self.fileName)
+		self.assertEqual('<div>\r\n<h2>\r\nEducation\r\n</h2>\r\n<ul>\r\n<li>\r\nUniversity of Penn, Town, PA, USA - Doctor of Coding\r\n</li>\r\n<li>\r\nBrigham Young University, Provo, UT, USA - Bachelor of Pinkberry\r\n</li>\r\n</ul>\r\n</div>', edu)
+
+	def test_proj_section(self):
+		proj = proj_section(self.fileName)
+		self.assertEqual(proj, '<div>\r\n<h2>\r\nProjects\r\n</h2>\r\n<ul>\r\n<li>\r\n<p>\r\nBill.com, Philadelphia, PA - Lead bill collector.\r\n</p>\r\n</li>\r\n<li>\r\n<p>\r\nArmy General - deployed platoon to Arizona where we saved the world.\r\n</p>\r\n</li>\r\n</ul>\r\n</div>')
+
+	def test_course_section(self):
+		course = course_section(self.fileName)
+		self.assertEqual(course, '<div>\r\n<h3>\r\nCourses\r\n</h3>\r\n<span>\r\nProgramming Languages and Techniques, Statistics, Negotiations\r\n</span>\r\n</div>')
+		
+	def test_surround_block(self):
+		test = surround_block('div', 'test')
+		test2 = surround_block('', 'test')
+		self.assertEqual(test, '<div>\r\ntest\r\n</div>')
+		self.assertEqual(test2, '<>\r\ntest\r\n</>')
+
+	def test_list_dict_html(self):
+		html = list_dict_html(['one', 'two'], 'li', 'div')
+		html2 = list_dict_html(['one'], 'li', 'div')
+		html3 = list_dict_html(['one', 'two'], 'li', '')
+		html4 = list_dict_html(['one', 'two', 'three'], 'li', '')
+		html_empty = list_dict_html([], '', '')
+		self.assertEqual('<div>\r\n<li>\r\none\r\n</li>\r\n</div>\r\n<div>\r\n<li>\r\ntwo\r\n</li>\r\n</div>', html)
+		self.assertEqual('<div>\r\n<li>\r\none\r\n</li>\r\n</div>', html2)
+		self.assertEqual('<li>\r\none\r\n</li>\r\n<li>\r\ntwo\r\n</li>', html3)
+		self.assertEqual('', html_empty)
+		self.assertEqual('<li>\r\none\r\n</li>\r\n<li>\r\ntwo\r\n</li>\r\n<li>\r\nthree\r\n</li>', html4)
+			
+	def test_cleanList(self):
+		clean = cleanList(['foo\r', 'bar\r\n', '', 'foobar'])
+		self.assertEqual(['foo', 'bar', 'foobar'], clean)
+		self.assertEqual([], cleanList([]))
 		
 	def test_check_ed(self):
 		edu = checkEd(self.fileName, 'Doctor')
-		self.assertEqual('University of Penn, Town, PA, USA - Doctor of Coding\r\n', edu)
+		self.assertEqual('University of Penn, Town, PA, USA - Doctor of Coding\n', edu)
+	
+	def test_combine(self):
+		combine = combine_html('html1', 'html2')
+		self.assertEqual('html1\r\nhtml2', combine)
 		
-		
+	def test_tie_body_header(self):
+		header = tie_body_header('<h2>\r\nEducation\r\n</h2>', 'One, Two', 'div')
+		self.assertEqual(header, '<div>\r\n<h2>\r\nEducation\r\n</h2>\r\n<div>\r\nOne, Two\r\n</div>\r\n</div>')
+	
+
 unittest.main()
